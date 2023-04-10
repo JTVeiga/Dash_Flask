7615966526f790b0210c5b4852be9176c7ffcb91
JTVeiga <jackstveiga@hotmail.com>
2023-04-10 16:28:27 -0300

    fix: update add.py graphs

--- app/main.py
+++ app/main.py
@@ -11,27 +11,60 @@ dash_app.config.suppress_callback_exceptions = True
 
 dash_app.layout = html.Div(
     children=[
-        html.H1(children="FreeBirds Crew"),
-        html.Div(children="""Docker Conatiner Running DASH WebApp"""),
+        html.H1(children="TELA DE INDICADORES - ALFA3-BD"),
+        html.Div(children="""PERCENTUAL DE ESTUDANTES POR PADRÃO DE LEITURA"""),
         dcc.Graph(
-            id="example-graph",
+            id="example-graph1",
             figure={
                 "data": [
-                    {"x": [1, 2, 3], "y": [4, 1, 2], "type": "bar", "name": "Like"},
+                    {"x": ["Fluente", "Iniciante", "Pré-Leitor"], "y": [18.3, 47.2, 21.2], "type": "bar", "name": "MUNICIPAL"},
                     {
-                        "x": [1, 2, 3],
-                        "y": [2, 4, 5],
+                        "x": ["Fluente", "Iniciante", "Pré-Leitor"],
+                        "y": [23.6, 46.2, 18.9],
                         "type": "bar",
-                        "name": "Comment",
+                        "name": "ESTADUAL",
                     },
                 ],
-                "layout": {"title": "Like Vs Comment Dash Visualization"},
+                "layout": {"title": " Velocidade Por Minuto"},
+            },
+        ),
+
+        html.Div(children="""PERCENTUAL DE ESTUDANTES QUE NÃO CONSEGUEM LER"""),
+        dcc.Graph(
+            id="example-graph2",
+            figure={
+                "data": [
+                    {"x": ["Texto", "Pseudopalavras", "Palavras"], "y": [14.8, 9.3, 8.6], "type": "bar", "name": "MUNICIPAL"},
+                    {
+                        "x": ["Texto", "Pseudopalavras", "Palavras"],
+                        "y": [10.9, 7.1, 6.7],
+                        "type": "bar",
+                        "name": "ESTADUAL",
+                    },
+                ],
+                "layout": {"title": " Velocidade Por Minuto"},
+            },
+        ),
+
+        html.Div(children="""QUALIDADE DA LEITURA"""),
+        dcc.Graph(
+            id="example-graph3",
+            figure={
+                "data": [
+                    {"x": ["Texto", "Pseudopalavras", "Palavras"], "y": [22, 13, 96], "type": "bar", "name": "MUNICIPAL"},
+                    {
+                        "x": ["Texto", "Pseudopalavras", "Palavras"],
+                        "y": [45, 33, 67],
+                        "type": "bar",
+                        "name": "ESTADUAL",
+                    },
+                ],
+                "layout": {"title": " Precisão de Acertos ou Erros"},
             },
         ),
     ]
+
 )
-
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True, port=80)
